488aefa05c08e31308b17ea17a23cb7b13bb517d
Mauricio Torres <otarksk@gmail.com>
2023-04-26 21:56:19 -0300

    Finish basic bot with buttons

--- button_bot.py
+++ button_bot.py
@@ -2,13 +2,16 @@ from config.config import *
 import telebot
 from telebot.types import ReplyKeyboardMarkup # This is to create buttons
 from telebot.types import ForceReply # To quote a message
+from telebot.types import ReplyKeyboardRemove # To remove buttons
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+users = {}
 
 #
 @bot.message_handler(commands=['start', 'help'])
 def cmd_start(message):
-    bot.send_message(message.chat.id, "How is your name?")
+    markup = ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, "How is your name?", reply_markup=markup)
 
 @bot.message_handler(commands=['replay'])
 def replay_message(message):
@@ -17,9 +20,10 @@ def replay_message(message):
     bot.register_next_step_handler(msg, ask_age)
 
 def ask_age(message):
-    name = message.text
+    users[message.chat.id] = {}
+    users[message.chat.id]['name'] = message.text
     markup = ForceReply()
-    msg = bot.send_message(message.chat.id, "How old are you ?" + name, reply_markup=markup)
+    msg = bot.send_message(message.chat.id, "How old are you ?", reply_markup=markup)
     bot.register_next_step_handler(msg, ask_sex)
 
 def ask_sex(message):
@@ -28,6 +32,7 @@ def ask_sex(message):
         msg = bot.send_message(message.chat.id, "ERROR: Please respond a valid age. \n how old are you?")
         bot.register_next_step_handler(msg, ask_sex)
     else:
+        users[message.chat.id]['age'] = int(message.text)
         markup = ReplyKeyboardMarkup(
             one_time_keyboard=True,
             input_field_placeholder='Click here',
@@ -41,6 +46,17 @@ def save_data(message):
     if message.text != "Man" and message.text != "woman":
         msg = bot.send_message(message.chat.id, "ERROR: Sex not valid. \n which is your sex?")
         bot.register_next_step_handler(msg, save_data)
+    else:
+        users[message.chat.id]['sex'] = message.text
+        text = 'Printed data:\n'
+        text += f'<code>Name:</code> {users[message.chat.id]["name"]}\n'
+        text += f'<code>Age :</code> {users[message.chat.id]["age"]}\n'
+        text += f'<code>Sex :</code> {users[message.chat.id]["sex"]}\n'
+        markup = ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, text, parse_mode='html', reply_markup=markup)
+        print(users)
+        #To delet dictionary
+        del users[message.chat.id]
 
 
 if __name__ == '__main__':
